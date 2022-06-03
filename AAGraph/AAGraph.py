ce4a7d50e54e3f1a6962d9ebba82cadf8daed513
miksubv <mv104@hotmail.com>
2022-06-03 12:37:05 +0200

    Cleaned AAGraph.py

--- AAGraph/AAGraph.py
+++ AAGraph/AAGraph.py
@@ -1,11 +1,94 @@
 # -*- coding: utf-8 -*-
 """
 @author: mlv
+
+Generates networkx graphs to be used by AAView to make graph plots for display.
+
 """
 
-from AAModule import AAModule
 from AAModuleTree import AAModuleTree
 import networkx as nx
+
+def dependencies_digraph_from_roots(roots, external_module_roots = {}):
+    '''
+    Construct a networkx DiGraph based on the modules and imports found in
+    the input collection of trees.
+
+    Parameters
+    ----------
+    roots : collection of trees
+        Description of the target system modules
+    external_module_roots : collection of trees
+        Description of the external packages
+
+    Returns
+    -------
+    networkx.DiGraph.
+    Resulting directed graph.
+    '''
+    G = nx.DiGraph()
+    for module_description in AAModuleTree.traverse_modules(roots):
+        module_name = module_description.full_name
+        if module_name in G.nodes():
+            print("!!!!!!!!!!!!!!! dependencies_digraph_from_roots: Duplicate module name: " + module_name)
+        G.add_node(module_name)
+    for module_description in AAModuleTree.traverse_modules(external_module_roots):
+        module_name = module_description.full_name
+        if module_name in G.nodes():
+            print("!!!!!!!!!!!!!!! dependencies_digraph_from_roots: Duplicate external module name " + module_name)
+        G.add_node(module_name)
+    for module_description in AAModuleTree.traverse_modules(roots):
+        for imported_module_full_name in module_description.imports:
+            if G.has_node(imported_module_full_name):  # Ignore imported modules not in the graph
+                G.add_edge(module_description.full_name, imported_module_full_name)
+    return G
+    
+        
+
+def dump_digraph(G):
+    '''
+    Debug function.
+    Print a list of all nodes and edges in a DiGraph.
+
+    Parameters
+    ----------
+    G : networkx.DiGraph
+        Graph to dump
+
+    Returns
+    -------
+    None.
+    '''
+    print(".................................................")
+    print("Dumping digraph")
+    print("Nodes:")
+    for node in G.nodes():
+        print(node)
+    print("Edges:")
+    for edge in G.edges():
+        print("From " + edge[0] + " To " + edge[1])
+
+
+'''
+    The remainder of this file is not used for the report.
+    It contains experiments that where ultimately not used.
+'''
+'''
+from AAModule import AAModule
+
+def rest_api_digraph(modules):
+    r_a_name = "Rest API"
+    G = nx.DiGraph()
+    G.add_node(r_a_name)
+    for module in modules:
+        for route in module.rest_api_routes:
+            print("Adding" + module.full_name)
+            G.add_node(module.full_name)
+            if route[0] == "GET":
+                G.add_edge(module.full_name, r_a_name)
+            if route[0] == "POST":
+                G.add_edge(r_a_name, module.full_name)
+    return G
 
 # Create a graph with all modules and dependencies between them
 def dependencies_graph(modules):
@@ -17,7 +100,6 @@ def dependencies_graph(modules):
         for each in module.imports:
             G.add_edge(module_name, each)
     return G
-
 # Create a graph with directed dependencies based on imports
 def dependencies_digraph(modules):
     G = nx.DiGraph()
@@ -68,58 +150,7 @@ def isolation_graph(modules, depth):
     #             import_depth = AAModule.relative_module_level(trunc_imported_module_name, imported_module)
     #             if G.has_edge(trunc_module_name, trunc_imported_module_name):
     #                 abe = False
-                    
 
-def dependencies_digraph_from_roots(roots, external_module_roots = {}):
-    G = nx.DiGraph()
-    for module_description in AAModuleTree.traverse_modules(roots):
-        module_name = module_description.full_name
-        if module_name in G.nodes():
-            print("!!!!!!!!!!!!!!! dependencies_digraph_from_roots: Duplicate module name: " + module_name)
-        G.add_node(module_name)
-    for module_description in AAModuleTree.traverse_modules(external_module_roots):
-        module_name = module_description.full_name
-        if module_name in G.nodes():
-            print("!!!!!!!!!!!!!!! dependencies_digraph_from_roots: Duplicate external module name " + module_name)
-        G.add_node(module_name)
-    for module_description in AAModuleTree.traverse_modules(roots):
-#        for imported_module_description in module_description.imported_modules:
-#            if not imported_module_description:
-#                print("oops")
-        for imported_module_full_name in module_description.imports:
-#            if G.has_node(imported_module_description.full_name):  # Ignore imported modules not in the graph
-#                G.add_edge(module_description.full_name, imported_module_description.full_name)
-            if G.has_node(imported_module_full_name):  # Ignore imported modules not in the graph
-                G.add_edge(module_description.full_name, imported_module_full_name)
-    return G
-    
-        
-
-def dump_digraph(G):
-    print(".................................................")
-    print("Dumping digraph")
-    print("Nodes:")
-    for node in G.nodes():
-        print(node)
-    print("Edges:")
-    for edge in G.edges():
-        print("From " + edge[0] + " To " + edge[1])
-
-def rest_api_digraph(modules):
-    r_a_name = "Rest API"
-    G = nx.DiGraph()
-    G.add_node(r_a_name)
-    for module in modules:
-        for route in module.rest_api_routes:
-            print("Adding" + module.full_name)
-            G.add_node(module.full_name)
-            if route[0] == "GET":
-                G.add_edge(module.full_name, r_a_name)
-            if route[0] == "POST":
-                G.add_edge(r_a_name, module.full_name)
-    return G
-            
-            
 # a filter takes an DG as input and returns another DG
 def keep_nodes(inputGraph, condition):
     result = nx.DiGraph()
@@ -159,4 +190,5 @@ def does_node_belong_to_module(full_node_name, full_module_name):
 
 def select_nodes_from_module(G, full_module_name):
     return keep_nodes(G, lambda node : does_node_belong_to_module(node, full_module_name))
+'''                    
         
